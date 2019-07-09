f9fd25a4310a3c665b0bf0c20b5644097d18b00f
Your Name <you@example.com>
2019-07-09 20:38:27 -0300

    permissões implementadas

--- tew/views.py
+++ tew/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 
 def home(request):
     return render(request, 'home.html',{})
-
+@login_required
 def list(request):
     users = User.objects.all()
     return render (request,'list.html',{'users':users})
 
-def user_delete(request,id):
+@permission_required("user_delete")
+def user_delete(request, id):
     user = User.objects.get(pk=id)
     user.delete()
-    return render(request,'list.html')
+    return redirect("/home/list/")
+
