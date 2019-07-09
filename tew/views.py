d49518c8f0ff45809736e204fcafb61b608b7160
Your Name <you@example.com>
2019-07-09 18:45:41 -0300

    criei os usuários

--- tew/views.py
+++ tew/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 
 def home(request):
     return render(request, 'home.html',{})
+
+def list(request):
+    users = User.objects.all()
+    return render (request,'list.html',{'users':users})
